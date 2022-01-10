882535c72b127fe2efb5a509143d646102653c75
Joe Devine <joedevine@uk.ibm.com>
2022-01-10 20:43:41 +0000

    Update Lambda to include dummy data

--- lib/authz-demo-stack.ts
+++ lib/authz-demo-stack.ts
@@ -1,15 +1,11 @@
 import * as cdk from '@aws-cdk/core';
 import * as apigw from '@aws-cdk/aws-apigateway';
 import * as lambda from '@aws-cdk/aws-lambda';
-
 export class AuthzDemoStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const api = new apigw.RestApi(this, 'ExampleApi', {
-      restApiName: 'ExampleApi',
-      description: 'Example OPA API',
-    });
+    // *** Customer Authoriser Setup ***
 
     const opaCustomAuthLambda = new lambda.Function(this, 'OpaAuthorizer', {
       runtime: lambda.Runtime.GO_1_X,
@@ -30,6 +26,13 @@ export class AuthzDemoStack extends cdk.Stack {
         ],
       },
     );
+
+    // *** REST Endpoint Setup ***
+
+    const api = new apigw.RestApi(this, 'ExampleApi', {
+      restApiName: 'ExampleApi',
+      description: 'Example OPA API',
+    });
 
     const example = api.root.addResource('example');
     const example_handler = new lambda.Function(this, 'example', {
